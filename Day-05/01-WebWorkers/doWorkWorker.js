55bae4d4231520a1a1745824cd0b1117ff3f31fe
tkmagesh <tkmagesh77@gmail.com>
2014-12-12 11:28:20 +0530

    Worker with progress

--- Day-05/01-WebWorkers/doWorkWorker.js
+++ Day-05/01-WebWorkers/doWorkWorker.js
@@ -1,16 +1,24 @@
 function doWork(){
-	for(var i=0;i<10000;i++)
+	for(var i=0;i<10000;i++){
 		for(var j=0;j<10000;j++)
 			for(var k=0;k<100;k++){
 
 			}
+		if (i%100 === 0){
+			//console.log(i/100);
+			self.postMessage({
+				type : "progress",
+				percentCompleted : i/100
+			});
+		}
+	}
 }
 self.addEventListener("message", onMessageFromMainWorker);
 function onMessageFromMainWorker(msgEvt){
 	var message = msgEvt.data;
 	if (message === "start"){
 		doWork();
-		self.postMessage("completed");
+		self.postMessage({type : "completed"});
 		console.log("work completed [from worker]");
 	} else {
 		console.log("unknown message ", msgEvt);
